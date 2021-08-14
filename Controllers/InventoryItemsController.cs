643adf93ef767a027831f7dd24eaccb2ab52dd17
barmao <bryanbarmao@gmail.com>
2021-08-14 23:03:36 +0300

    removed unused code

--- Controllers/InventoryItemsController.cs
+++ Controllers/InventoryItemsController.cs
@@ -14,20 +14,19 @@ namespace kumbuka_api.Controllers
     [ApiController]
     public class InventoryItemsController : ControllerBase
     {
-        //private readonly InventoryItemContext _context;
         private readonly InventoryItemsService _inventoryItemsService;
 
         public InventoryItemsController(InventoryItemsService inventoryItem)
         {
-            //_context = context;
             _inventoryItemsService = inventoryItem;
         }
 
-
+        // GET: api/InventoryItems
         [HttpGet]
         public ActionResult<List<InventoryItem>> Get() => _inventoryItemsService.Get();
 
-        // GET: api/InventoryItems
+        
+        //// GET: api/InventoryItems/5
         [HttpGet("{id:length(24)}", Name = "GetInventoryItems")]
         public ActionResult<InventoryItem> Get(string id)
         {
@@ -41,6 +40,7 @@ namespace kumbuka_api.Controllers
             return item;
         }
 
+        //// POST: api/InventoryItems
         [HttpPost]
         public ActionResult<InventoryItem> Create(InventoryItem item)
         {
@@ -49,6 +49,7 @@ namespace kumbuka_api.Controllers
             return CreatedAtRoute("GetBook", new { id = item.Id.ToString() }, item);
         }
 
+        //// PUT: api/InventoryItems/5
         [HttpPut("{id:length(24)}")]
         public IActionResult Update(string id, InventoryItem itemIn)
         {
@@ -64,6 +65,7 @@ namespace kumbuka_api.Controllers
             return NoContent();
         }
 
+        //// DELETE: api/InventoryItems/5
         [HttpDelete("{id:length(24)}")]
         public IActionResult Delete(string id)
         {
@@ -78,83 +80,5 @@ namespace kumbuka_api.Controllers
 
             return NoContent();
         }
-
-        //// GET: api/InventoryItems/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<InventoryItem>> GetInventoryItem(long id)
-        //{
-        //    var inventoryItem = await _context.InventoryItems.FindAsync(id);
-
-        //    if (inventoryItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return inventoryItem;
-        //}
-
-        //// PUT: api/InventoryItems/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutInventoryItem(long id, InventoryItem inventoryItem)
-        //{
-        //    if (id != inventoryItem.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(inventoryItem).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!InventoryItemExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/InventoryItems
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<InventoryItem>> PostInventoryItem(InventoryItem inventoryItem)
-        //{
-        //    _context.InventoryItems.Add(inventoryItem);
-        //    await _context.SaveChangesAsync();
-
-        //    //return CreatedAtAction("GetInventoryItem", new { id = inventoryItem.Id }, inventoryItem);
-        //    return CreatedAtAction(nameof(GetInventoryItem), new { id = inventoryItem.Id }, inventoryItem);
-        //}
-
-        //// DELETE: api/InventoryItems/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteInventoryItem(long id)
-        //{
-        //    var inventoryItem = await _context.InventoryItems.FindAsync(id);
-        //    if (inventoryItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.InventoryItems.Remove(inventoryItem);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
-
-        //private bool InventoryItemExists(long id)
-        //{
-        //    return _context.InventoryItems.Any(e => e.Id == id);
-        //}
     }
 }
